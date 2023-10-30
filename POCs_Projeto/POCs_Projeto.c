79779d89d3e46c0a0fae2594acaad9161204b212
ChristianDev123 <christian.lima999@gmail.com>
2023-10-29 21:51:03 -0300

    poc ready

--- POCs_Projeto/POCs_Projeto.c
+++ POCs_Projeto/POCs_Projeto.c
@@ -15,10 +15,58 @@ int** setScreenConfig(DisplaySettings* Display) {
                 screen[i] = malloc(Display->Width * sizeof(int*));
             }
         }
-
         return screen;
 }
 
+void tracejarLinha(int** matriz, int x1, int y1, int x2, int y2) {
+    int dx, dy, p, x, y;
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    x = x1;
+    y = y1;
+
+    p = 2 * dy - dx;
+
+    while (x < x2) {
+        if (p >= 0) {
+            matriz[y][x] = 2;
+            y++;
+            p = p + 2 * dy - 2 * dx;
+        }
+        else {
+            matriz[y][x] = 2;
+            p = p + 2 * dy;
+        }
+        printf("x:%d y:%d\n", x, y);
+        x++;
+    }
+}
+
+void createLine(DisplaySettings* Display){ 
+    int* pos = malloc(sizeof(int)*4);
+    int index = 0;
+    for (int i = 0; i < Display->Height; i++) {
+        for (int j = 0; j < Display->Width; j++) {
+            if (Display->screen[i][j] == 3 && pos) {
+                pos[index++] = j;
+                pos[index++] = i;
+
+                if (Display->screen[i][j] == 3)
+                    Display->screen[i][j] = 4;
+                
+                al_draw_circle(j,i,4, al_map_rgb(0, 255, 0),4);
+            }
+        }
+    }
+    
+    tracejarLinha(Display->screen, pos[0], pos[1], pos[2], pos[3]);
+    al_draw_line(pos[0], pos[1], pos[2], pos[3], al_map_rgb(0, 0, 255), 4);
+    index = 0;
+
+    free(pos);
+}
 
 int main()
 {
@@ -60,6 +108,7 @@ int main()
         bool redraw = true;
         ALLEGRO_EVENT event;
         al_start_timer(timer);
+        int links = 0;
 
         while (true)
         {
@@ -77,6 +126,18 @@ int main()
                 case ALLEGRO_EVENT_MOUSE_AXES:
                     Mouse->x = event.mouse.x;
                     Mouse->y = event.mouse.y;
+                    break;
+                case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+                    if (event.mouse.button == 1 && Display->screen) {
+                        int x = Mouse->x;
+                        int y = Mouse->y;
+                        Display->screen[--y][--x] = 3;
+                        links++;
+                        if (links == 2) {
+                            createLine(Display);
+                            links = 0;
+                        }
+                    }
                     break;
             }
 
